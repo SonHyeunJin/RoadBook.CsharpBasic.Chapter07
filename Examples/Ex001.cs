acfbb4eff17fdd43c2ea82902489beb20ff384a3
sonhyeunjin <dksjejrl123@naver.com>
2024-08-28 01:16:29 +0900

    초기 커밋.

---
+++ Examples/Ex001.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadBook.CsharpBasic.Chapter07.Examples.Examples
+{
+    internal class Ex001
+    {
+        public void Run()
+        {
+            Console.Write("숫자를 입력 하세요 : ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            // intput value : "one"
+
+            Console.WriteLine("입력된 숫자는 {0}", number);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoadBook.CsharpBasic.Chapter07.Examples.Examples;
+
+namespace RoadBook.CsharpBasic.Chapter07.Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex001 examples = new Ex001();
+            examples.Run();
+        }
+    }
+}
